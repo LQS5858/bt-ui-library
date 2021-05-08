0f87773112090233b2c1acca82b40bb0ec796bbc
lqs5858@163.com <LQS5858@163.COM>
2021-05-08 16:29:39 +0800

    ci cd

--- styleguide.config.js
+++ styleguide.config.js
@@ -5,7 +5,7 @@ module.exports = {
   defaultExample: false, // 是否使用默认样例
   usageMode: 'expand', // 是否展开用法
   exampleMode: 'expand', // 是否展开示例代码
-  styleguideDir: 'styleguide', // 打包的目录
+  styleguideDir: 'docs', // 打包的目录
   codeSplit: true, // 打包时是否进行分片
   skipComponentsWithoutExample: false // 是否跳过没有样例的组件
 }
